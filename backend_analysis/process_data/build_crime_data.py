106bcd01f162ec59071e903f56f9d7aac9aa06d4
zchadick <zack.chadick@gmail.com>
2014-06-18 23:40:43 -0700

    added crimedb processing

--- backend_analysis/process_data/build_crime_data.py
+++ backend_analysis/process_data/build_crime_data.py
@@ -14,8 +14,9 @@ from sqlalchemy.types           import Float, Date
 from sqlalchemy.orm             import sessionmaker
 from dateutil.relativedelta     import relativedelta
 
-
-table_name = 'pricedata'
+table_name = 'crimedata'
+bad_crimes = ['SIMPLE ASSAULT','ROBBERY','AGGRAVATED ASSAULT','ARSON']
+bc_scale   = 5
 
 def get_spacing_information():
     # connect to the database to get how far apart 'points' are
@@ -30,50 +31,74 @@ def get_spacing_information():
 
 def get_timestamps():
     # connect to the database and get listing of times
-    conn  = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='housedb')
+    conn  = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='crimedb')
     cur   = conn.cursor()
-    cur.execute("SELECT DISTINCT h_date from housedb3;")
+    cur.execute("SELECT DISTINCT datecr from crimedb1;")
     data  = cur.fetchall()
     conn.close()
 
-    dates = sorted(data,key = lambda x: x[0])
+    t_dates  = sorted(data,key = lambda x: x[0])
+
+    d_start  = t_dates[0][0]
+    d_end    = t_dates[-1][0]
+
+    st_year  = d_start.year
+    st_month = d_start.month
+
+    en_year  = d_end.year
+    en_month = d_end.month
+
+    date_s   = datetime.date(st_year,st_month,1)
+    date_e   = datetime.date(en_year,en_month,1)
+
+    dates    = []
+    cur_date = date_s
+    dates.append(cur_date)
+
+    while cur_date!=date_e:
+        cur_date = cur_date + relativedelta(months=1)
+        dates.append(cur_date)
+
     return dates
 
-def cycle_through_times(dates,spacing,sql_str1,lat,lon,place_id):
+def cycle_through_times(dates,spacing,sql_str1,lat,lon,place_id,bad_crimes,bc_scale):
     # connect to the database and start querying
 
-    conn   = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='housedb')
+    conn   = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='crimedb')
     cur    = conn.cursor()
     p_data = []
 
     for row in dates:
         # cycle through all the dates you've found
-        date_year  = row[0].year
-        date_month = row[0].month
+        date_year  = row.year
+        date_month = row.month
         date_day   = 1
         date_start = datetime.date(date_year,date_month,date_day)
         date_end   = date_start + relativedelta(months=1)
         date_st_s  = str(date_start)
         date_en_s  = str(date_end)
-        sql_quer   = "SELECT houseid,price,h_lat,h_long FROM housedb3"  \
-                      + " WHERE (h_date>="'"{0}"'" AND h_date<"'"{1}"'") \
-                      AND h_room=2;".format(date_st_s,date_en_s)
+        sql_quer   = "SELECT typecr, latcr, loncr FROM crimedb1"  \
+                      + " WHERE (datecr>="'"{0}"'" AND datecr<"'"{1}"'");".format(date_st_s,date_en_s)
         cur.execute(sql_quer)
         data = cur.fetchall()
 
         distance = []
-        prices   = []
+        cr_type  = []
 
         for vals in data:
-            # cycle through each house listing
+            # cycle through each crime listing
             # find the distance to point and impose a gaussian
-            dist  = math.sqrt(math.pow(vals[2]-lat,2)+math.pow(vals[3]-lon,2))
+            dist  = math.sqrt(math.pow(vals[1]-lat,2)+math.pow(vals[2]-lon,2))
             d_val = math.exp(-math.pow(dist,2)/(2*math.pow(spacing/2.3548,2)))
 
             distance.append(d_val)
-            prices.append(d_val*vals[1])
 
-        p_data.append(round(sum(prices)/sum(distance)))
+            if vals[0] in bad_crimes:
+                cr_type.append(d_val*bc_scale)
+            else:
+                cr_type.append(d_val)
+
+        p_data.append(sum(cr_type))
 
     # connect to SQL database
 
@@ -95,7 +120,7 @@ def cycle_through_times(dates,spacing,sql_str1,lat,lon,place_id):
     cur.close()
     conn.close()
 
-def cycle_through_locations(dates,spacing,sql_str1):
+def cycle_through_locations(dates,spacing,sql_str1,bad_crimes,bc_scale):
     # connect to the database to get how far apart 'points' are
     conn  = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='citydata')
     cur   = conn.cursor()
@@ -105,7 +130,7 @@ def cycle_through_locations(dates,spacing,sql_str1):
     conn.close()
 
     for num in data:
-        cycle_through_times(dates,spacing,sql_str1,num[2],num[3],num[0])
+        cycle_through_times(dates,spacing,sql_str1,num[2],num[3],num[0],bad_crimes,bc_scale)
 
 # initial checks
 
@@ -126,8 +151,8 @@ create_str.append('CREATE TABLE {0} (id INTEGER PRIMARY KEY AUTO_INCREMENT UNIQU
 create_str.append('place_id INTEGER')
 
 for d_dat in dates:
-    date_year  = d_dat[0].year
-    date_month = d_dat[0].month
+    date_year  = d_dat.year
+    date_month = d_dat.month
     date_day   = 1
     date_start = datetime.date(date_year,date_month,date_day)
 
@@ -156,7 +181,7 @@ conn.close()
 
 # cycle through all locations
 
-cycle_through_locations(dates,spacing,sql_str1)
+cycle_through_locations(dates,spacing,sql_str1,bad_crimes,bc_scale)
 
 # get the city-wide averages
 
@@ -168,24 +193,31 @@ s_data = []
 for row in dates:
     # cycle through all the dates you've found
 
-    conn   = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='housedb')
-    cur    = conn.cursor()
+    cr_inds = 0
 
-    date_year  = row[0].year
-    date_month = row[0].month
+    conn    = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='crimedb')
+    cur     = conn.cursor()
+
+    date_year  = row.year
+    date_month = row.month
     date_day   = 1
     date_start = datetime.date(date_year,date_month,date_day)
     date_end   = date_start + relativedelta(months=1)
     date_st_s  = str(date_start)
     date_en_s  = str(date_end)
-    sql_quer   = "SELECT price FROM housedb3"  \
-                  + " WHERE (h_date>="'"{0}"'" AND h_date<"'"{1}"'") \
-                  AND h_room=2;".format(date_st_s,date_en_s)
+    sql_quer   = "SELECT typecr FROM crimedb1"  \
+                  + " WHERE (datecr>="'"{0}"'" AND datecr<"'"{1}"'");".format(date_st_s,date_en_s)
 
     cur.execute(sql_quer)
     data = cur.fetchall()
-    p_data.append(np.mean(data))
-    s_data.append(np.std(data))
+
+    for ind in data:
+        if ind[0] in bad_crimes:
+            cr_inds = cr_inds + bc_scale
+        else:
+            cr_inds = cr_inds + 1
+
+    p_data.append(cr_inds)
 
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='citydata')
 cur  = conn.cursor()
@@ -193,17 +225,8 @@ cur  = conn.cursor()
 str_copy = sql_str1
 
 sql_str1 = str_copy
-sql_str1 = sql_str1 + str(9998) + ','
-for numel in p_data:
-    sql_str1 = sql_str1 + str(numel) + ','
-myquery = sql_str1[:-1] + ')'
-
-cur.execute(myquery)
-conn.commit()  
-
-sql_str1 = str_copy
 sql_str1 = sql_str1 + str(9999) + ','
-for numel in s_data:
+for numel in p_data:
     sql_str1 = sql_str1 + str(numel) + ','
 myquery = sql_str1[:-1] + ')'
 
