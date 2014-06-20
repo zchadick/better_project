9b3dd5678613a5999981ea36387c3c7a7852bcfe
zchadick <zack.chadick@gmail.com>
2014-06-19 18:41:02 -0700

    "MAJOR WEBSITE CHANGES! IT WORKS!!!"

--- app/views.py
+++ app/views.py
@@ -20,43 +20,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# To create a database connection, add the following
-# within your view functions:
-# con = con_db(host, port, user, passwd, db)
-
-#from app.helpers.database import con_db, query_db
-#from app.helpers.filters import format_currency
-
- 
-#def get_db():
-#	print "Getting DB"
-#	top = _app_ctx_stack.top
-#	if not hasattr(top, 'home_kitchen_db'):
-#		top.home_kitchen_db = pymysql.connect(host="localhost", user="root", db = "semfundc_zidisha")
-#	return top.home_kitchen_db
-#
-#def query_db(query):
-#	sys.stderr.write("Querying Database with: "  + query)
-#	cursor = get_db().cursor()
-#	cursor.execute(query)
-#	return cursor.fetchall()
-#
-#def running_average(data):
-#	result = []
-#	index = 0
-#	total = 0
-#	for x in data:
-#		index += 1
-#		total += x[1]
-#		result.append([x[0], total/index])
-#	return result	
-
 @app.route('/')
 def index():
-	return render_template('index.html')
-
-
+	return render_template('index.html',ind=9999)
         
 @app.route('/out', methods=['POST'])
 
@@ -111,12 +77,6 @@ def out():
                                             valid   = valid,   out_text = out_text, db_id   = db_id,
                                             db_name = db_name, db_lat   = db_lat,   db_lon  = db_lon,
                                             ind     = ind)
-        
-        
-	#data = query_db(query)
-	#data = running_average(data)
-	#formatted_data = map(lambda d: {'time': d[0], 'rating':d[1]}, data)
-	#return jsonify(reviews = formatted_data)
 	
 def parse_the_location(p_lat,p_lon):
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='citydata')
@@ -181,16 +141,75 @@ def getPlot():
     data  = list(data[0][2:-1])
     x_dat = range(1,len(data)+1)
 
-    fig, ax = plt.subplots()
-    plt.plot(x_dat,data)
-    plt.title('CRIME NUMBERS FOR ALL OF SAN FRANCISCO')
-    plt.xlabel('DATE')
-    plt.xticks(x_dat[0::12])
-    ax.set_xticklabels(n_labels[0::12])
-    plt.ylabel('# of CRIMES')
-    plt.grid(True)
-    plt.figtext(0.995, 0.01, 'crimespotting.org',ha='right', va='bottom')
-    plt.tight_layout(pad=1) 
+    if str(place_id)==str(9999):
+        p_title = 'CRIME NUMBERS FOR ALL OF SAN FRANCISCO'
+    else:
+        p_title = 'CRIME NUMBERS FOR LOCAL NEIGHBORHOOD'
+
+
+    fig = plt.figure(figsize=(9,12))
+    ax1 = fig.add_subplot(2,1,1)
+    ax1.plot(x_dat,data)
+    ax1.set_title(p_title,size=24)
+    ax1.set_xlabel('DATE',size=24)
+    ax1.set_xticks(x_dat[0::12])
+    ax1.set_xticklabels(n_labels[0::12])
+    ax1.set_ylabel('# of CRIMES',size=24)
+    ax1.grid(True)
+    fig.tight_layout(pad=1) 
+
+    db_name    = 'citydata'
+    table_name = 'pricedata'
+    place_id   = id
+
+    # get column names
+
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db=db_name)
+    cur  = conn.cursor()
+
+    sql_query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE \
+                TABLE_SCHEMA='{0}' AND TABLE_NAME='{1}';".format(db_name,table_name)
+
+    cur.execute(sql_query)
+    data  = cur.fetchall()
+
+    d_labels = list(data[2:-1])
+    n_labels = []
+
+    for text in d_labels:
+        xx = text[0]
+        n_labels.append('-'.join([xx[1:5],xx[5:7]]))
+
+    # get data
+
+    sql_query = 'SELECT * FROM {0} WHERE place_id = {1};'.format(table_name,place_id)
+    cur.execute(sql_query)
+    data  = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    data  = list(data[0][2:-1])
+    x_dat = range(1,len(data)+1)
+
+    if str(place_id)==str(9999):
+        p_title = 'AVERAGE SALE PRICE FOR ALL OF SAN FRANCISCO'
+    else:
+        p_title = 'AVERAGE SALE PRICE FOR LOCAL NEIGHBORHOOD'
+
+    print n_labels
+
+    newdata = [x/1000 for x in data]
+
+    ax2 = fig.add_subplot(2,1,2)
+    ax2.plot(x_dat,newdata)
+    ax2.set_title(p_title,size=24)
+    ax2.set_xlabel('DATE',size=24)
+    ax2.set_xticks(x_dat[0::12])
+    ax2.set_xticklabels(n_labels[0::12])
+    ax2.set_ylabel('SALE PRICE ($k)',size=24)
+    ax2.grid(True)
+    fig.text(0.995, 0.01, 'crimespotting.org and zillow.com',ha='right', va='bottom')
+    fig.tight_layout(pad=1) 
 
     img = StringIO()
     fig.savefig(img)
